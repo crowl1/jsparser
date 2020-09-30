919d5a0287eed78792d300faf2f473c2b31fd7ce
crowl1 <47753606+crowl1@users.noreply.github.com>
2020-09-30 23:53:39 +0300

    added filling to the table

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+from filling_table import filling
+
 HEADERS = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.63', 'accept': '*/*'}
 URL = 'https://www.imdb.com/chart/top'
 
@@ -25,9 +27,7 @@ def parser():
             'members' : item.find('a').get('title').split(',')
         }
     )
-    print(films)
-
-    
+    filling(films)
 
 
 parser()
